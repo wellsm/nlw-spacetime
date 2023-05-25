e57905da32107ba2481ab21fbc52b973e22d0cac
Well <wellingtonsdsm@gmail.com>
2023-05-25 00:11:47 -0300

    feat: eslint-prettier on mobile and fonts on web

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
-export default function App() { 
+export default function App() {
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">NLW Spacetime</Text>
       <StatusBar style="light" />
     </View>
-  );
+  )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1 className="">Hello World</h1>
+      <h1 className="font-alt">Hello World</h1>
+    </div>
+  )
 }
